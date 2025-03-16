cf9ab1d7dfbcd429e3307b5e575f90dd36572f21
Rahul <mangadivine.in@gmail.com>
2025-03-16 21:03:44 +0530

    Create right_half_pyramid_of_numbers.py

---
+++ right_half_pyramid_of_numbers.py
@@ -0,0 +1,9 @@
+#right half pyramid of numbers
+def right_half_pyramid_of_numbers(r):
+  print("Right half pyramid of numbers!")
+  for i in range(1, r + 1):
+      print(" " * (r - i), end=" ")
+      for j in range(1, i + 1):
+        print(j, end=" ")
+      print()
+right_half_pyramid_of_numbers(r)
