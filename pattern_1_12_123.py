cb0f43ce0269d6a60ef714a971b56f0859213c70
Rahul <mangadivine.in@gmail.com>
2025-03-16 21:01:50 +0530

    Create pattern_1_12_123.py

---
+++ pattern_1_12_123.py
@@ -0,0 +1,8 @@
+#pattern 1 12 123..
+def pattern_1_12_123(r):
+  print("Print pattern 1 12 123 1234 12345...!")
+  for i in range(1,r + 1):
+    for j in range(1,i + 1):
+      print(j, end = " ")
+    print()
+pattern_1_12_123(r)
